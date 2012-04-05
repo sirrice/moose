bef89b5023db71f1265988c82a19d349288540b1
eugene wu <sirrice@gmail.com>
2012-04-04 20:17:11 -0400

    modified/hacked registration code to work.  temporarily removed need to authenticate (everyone is auto authenticated)

--- registration/models.py
+++ registration/models.py
@@ -79,7 +79,7 @@ class RegistrationManager(models.Manager):
         """
 #        new_user = User.objects.create_user(username, email, password)
         new_user = create_user(email, password)
-        new_user.is_active = False
+        new_user.is_active = True
         new_user.save()
 
         registration_profile = self.create_profile(new_user)
