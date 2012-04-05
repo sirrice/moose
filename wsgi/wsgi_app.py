40d21d48ceef3f1fb312a8fb3f41c1ad8539bbbc
Adam Marcus <marcua@marcua.net>
2012-04-05 18:38:05 -0400

    yes

--- wsgi/wsgi_app.py
+++ wsgi/wsgi_app.py
@@ -3,7 +3,7 @@ import sys
 import os
 import django.core.handlers.wsgi
 sys.path.append('/home/django/')
-sys.path.append('/data/django/moose/')
+sys.path.append('/home/django/moose/')
 os.environ['DJANGO_SETTINGS_MODULE'] = 'moose.settings'
 application = django.core.handlers.wsgi.WSGIHandler()
 
